76725b1a3f91230652e3a8422a6f054c9d425963
danil-proger <92161604+danil-proger@users.noreply.github.com>
2021-12-17 22:20:47 +0300

    Create 2-C.cpp
    https://codeforces.com/group/PVbQ8eK2T4/contest/347382/submission/132966692
    https://codeforces.com/group/PVbQ8eK2T4/contest/347382/problem/C

---
+++ 2-C.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+pair<vector<int>, vector<int>> Partition(vector<int> A, long long pivot) {
+	pair<vector<int>, vector<int>> part;
+	for (int el : A) {
+		if (el < pivot) part.first.push_back(el);
+		if (el > pivot) part.second.push_back(el);
+	}
+	return part;
+}
+
+int QuickSelect(vector<int> A, int k) {
+	if (A.size() == 1) return A[0];
+	long long pivot = A[rand() % (A.size())];
+	pair<vector<int>, vector<int>> part;
+	part = Partition(A, pivot);
+	if (k < part.first.size()) return QuickSelect(part.first, k);
+	if (k < A.size() - part.second.size()) return pivot;
+	return QuickSelect(part.second, k - A.size() + part.second.size());
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cout.tie(0);
+	cin.tie(0);
+	int n, k;
+	cin >> n >> k;
+	vector<int> A(n);
+	for (int i = 0; i < n; i++) {
+		cin >> A[i];
+	}
+	cout << QuickSelect(A, k);
+}
