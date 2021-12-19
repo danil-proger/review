bf034788a0fd9d9420a711c5ab60863c7a6394c5
danil-proger <92161604+danil-proger@users.noreply.github.com>
2021-12-19 12:16:09 +0300

    Update 2-C.cpp
    https://codeforces.com/group/PVbQ8eK2T4/contest/347382/problem/C
    https://codeforces.com/group/PVbQ8eK2T4/contest/347382/submission/139873346

--- 2-C.cpp
+++ 2-C.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #include <fstream>
 #include <vector>
@@ -6,34 +6,38 @@
 
 using namespace std;
 
-pair<vector<int>, vector<int>> Partition(vector<int> A, long long pivot) {
-	pair<vector<int>, vector<int>> part;
-	for (int el : A) {
-		if (el < pivot) part.first.push_back(el);
-		if (el > pivot) part.second.push_back(el);
-	}
-	return part;
+int Partition(vector<int> &A, int l, int r) {
+    int value = A[(l + r) / 2];
+    swap(A[(l + r) / 2], A[r]);
+    int i = l;
+    int j = r-1;
+    while (i <= j) {
+        while (A[i] < value) ++i;
+        while (A[j] >= value) --j;
+        if (i >= j) break ;
+        swap(A[i++], A[j--]);
+    }
+    swap(A[i], A[r]);
+    return i;
 }
 
-int QuickSelect(vector<int> A, int k) {
-	if (A.size() == 1) return A[0];
-	long long pivot = A[rand() % (A.size())];
-	pair<vector<int>, vector<int>> part;
-	part = Partition(A, pivot);
-	if (k < part.first.size()) return QuickSelect(part.first, k);
-	if (k < A.size() - part.second.size()) return pivot;
-	return QuickSelect(part.second, k - A.size() + part.second.size());
+int QuickSelect(vector<int> &A, int k, int l, int r) {
+    if (A.size() == 1) return A[0];
+    int part = Partition(A, l, r);
+    if (k < part) return QuickSelect(A, k, l, part-1);
+    if (k == part) return A[part];
+    if (k > part) return QuickSelect(A, k, part+1, r);
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cout.tie(0);
-	cin.tie(0);
-	int n, k;
-	cin >> n >> k;
-	vector<int> A(n);
-	for (int i = 0; i < n; i++) {
-		cin >> A[i];
-	}
-	cout << QuickSelect(A, k);
+    ios_base::sync_with_stdio(false);
+    cout.tie(0);
+    cin.tie(0);
+    int n, k;
+    cin >> n >> k;
+    vector<int> A(n);
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+    cout << QuickSelect(A, k, 0, n - 1);
 }
